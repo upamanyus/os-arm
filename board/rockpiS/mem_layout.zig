f63943d1357db8a4378a0d5731dcad754367de82
Upamanyu Sharma <upamanyu@mit.edu>
2022-09-03 01:02:54 -0400

    Successfully boot on rock pi S!

--- board/rockpiS/mem_layout.zig
+++ board/rockpiS/mem_layout.zig
@@ -1,2 +1,2 @@
 // rock pi S
-pub const end: usize = 0xFF000000;
+pub const end: usize = 0x2000_0000;
--- kmem.zig
+++ kmem.zig
@@ -1,5 +1,6 @@
 const mem_layout = @import("mem_layout.zig");
 const panic = @import("panic.zig");
+const uart = @import("uart.zig");
 
 const pgbits = 12;
 pub const pgsize = (1 << pgbits);
@@ -14,9 +15,13 @@ pub fn init() void {
     // allocate single pages until curr_page is aligned with 16KB boundary
     mem_layout.init();
 
-    var addr: usize = mem_layout.start;
+    var addr: usize = pgup(mem_layout.start);
+    // uart.printf("Setting up linked list from 0x{0x} to 0x{1x}\n\r", .{ mem_layout.start, mem_layout.end });
 
     while (addr + pgsize < mem_layout.end) {
+        if ((addr / pgsize) % 1024 == 0) {
+            // uart.printf("Setting up page at 0x{0x}MB\n\r", .{addr / (1024 * 1024)});
+        }
         free(addr);
         addr += pgsize;
     }
